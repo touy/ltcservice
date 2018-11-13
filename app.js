4c8402e8070522066eac054f8e31c448d5332679
anousone <touya_ra@hotmail.com>
2018-11-13 11:27:15 +0700

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 let ltc;
+
 // fs.readFile('./ltc.key', 'utf8', function (err,data) {
 //   if (err) {
 //     return console.log(err);
